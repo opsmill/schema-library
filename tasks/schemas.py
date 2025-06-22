0a8286331ef25f8788942f5d087745eb7170332c
Pete Crocker <pete@petecrocker.com>
2025-06-22 10:45:13 +0100

    fix branching

--- tasks/schemas.py
+++ tasks/schemas.py
@@ -121,6 +121,132 @@ def stop(context: Context) -> None:
     context.run(f"{COMPOSE_COMMAND} down")
 
 
+def format_table(headers: list, rows: list):
+    """Generate a Markdown table."""
+
+    def escape_markdown(text: str) -> str:
+        # Escape special markdown characters
+        special_chars = [
+            "|",
+            "_",
+            "*",
+            "`",
+            "[",
+            "]",
+            "(",
+            ")",
+            "#",
+            "+",
+            "-",
+            ".",
+            "!",
+            "$",
+            "<",
+            ">",
+        ]
+
+        for char in special_chars:
+            text = text.replace(char, f"\\{char}")
+        return text
+
+    table = f"\n| {' | '.join(headers)} |\n"
+    table += f"| {' | '.join(['-' * len(header) for header in headers])} |\n"
+    for row in rows:
+        escaped_row = [escape_markdown(cell) for cell in row]
+        table += f"| {' | '.join(escaped_row)} |\n"
+    return table
+
+
+def format_data(header: str, data: dict):
+    if header == "choices":
+        choices = [choice.get("name", "") for choice in data.get(header, [])]
+        choices_str = ", ".join(choices)
+        return f"`{choices_str}`"
+
+    return str(data.get(header, ""))
+
+
+def generate_table_data(data: list):
+    headers = []
+    for attr in data:
+        for k in attr.keys():
+            if k not in headers:
+                headers.append(k)
+
+    rows = []
+    for attr in data:
+        row = [format_data(header, attr) for header in headers]
+        rows.append(row)
+
+    return headers, rows
+
+
+def generate_node_data(node: dict):
+    node_markdown = []
+    node_markdown.append(f"#### {node.get('name')}\n")
+    if node.get("description"):
+        node_markdown.append(f"- **Description:** {node.get('description')}")
+    if node.get("label"):
+        node_markdown.append(f"- **Label:** {node.get('label', '')}")
+    if node.get("icon"):
+        node_markdown.append(f"- **Icon:** {node.get('icon', '')}")
+    if node.get("menu_placement"):
+        node_markdown.append(
+            f"- **Menu Placement:** {node.get('menu_placement', '')}"
+        )
+    node_markdown.append(
+        f"- **Include in Menu:** {'✅' if node.get('include_in_menu') else '❌'}\n"
+    )
+    if node.get("order_by") or node.get("uniqueness_constraints"):
+        node_markdown.append("#### Ordering and Constraints\n")
+        node_markdown.append(
+            f"- **Order By:**{', '.join(node.get('order_by', []))}"
+        )
+        node_markdown.append(
+            f"- **Uniqueness Constraints:**{', '.join([' + '.join(c) for c in node.get('uniqueness_constraints', [])])}\n"
+        )
+
+    if attributes := node.get("attributes", []):
+        node_markdown.append("##### Attributes")
+        attribute_headers, attribute_rows = generate_table_data(attributes)
+        node_markdown.append(format_table(attribute_headers, attribute_rows))
+
+    if relationships := node.get("relationships", []):
+        node_markdown.append("#### Relationships")
+        relationship_headers, relationship_rows = generate_table_data(relationships)
+        node_markdown.append(format_table(relationship_headers, relationship_rows))
+    return node_markdown
+
+
+def handle_generics(generics):
+    content = ["### Generics\n"]
+    for generic in generics:
+        content.extend(generate_node_data(generic))
+    return content
+
+
+def handle_nodes(nodes):
+    content = ["### Nodes\n"]
+    for node in nodes:
+        content.extend(generate_node_data(node))
+    return content
+
+
+def handle_extensions(extensions):
+    content = ["### Extensions\n"]
+    for node in extensions.get("nodes", []):
+        content.append(f"#### {node.get('kind', '')}\n")
+        if attributes := node.get("attributes", []):
+            content.append("#### Attributes")
+            headers, rows = generate_table_data(attributes)
+            content.append(format_table(headers, rows))
+        if relationships := node.get("relationships", []):
+            content.append("#### Relationships")
+            headers, rows = generate_table_data(relationships)
+            content.append(format_table(headers, rows))
+    return content
+
+
 def generate_readme(schema, extension_dir: Path) -> list:
     schema_definition_files = {}
     for yml_file in extension_dir.glob("*.yml"):
@@ -139,126 +265,17 @@ def generate_readme(schema, extension_dir: Path) -> list:
     if attribution := schema.get("attribution", []):
         content.append(f"- **Attribution**: {attribution}\n")
 
-    def format_table(headers: list, rows: list):
-        """Generate a Markdown table."""
-
-        def escape_markdown(text: str) -> str:
-            # Escape special markdown characters
-            special_chars = [
-                "|",
-                "_",
-                "*",
-                "`",
-                "[",
-                "]",
-                "(",
-                ")",
-                "#",
-                "+",
-                "-",
-                ".",
-                "!",
-                "$",
-                "<",
-                ">",
-            ]
-
-            for char in special_chars:
-                text = text.replace(char, f"\\{char}")
-            return text
-
-        table = f"\n| {' | '.join(headers)} |\n"
-        table += f"| {' | '.join(['-' * len(header) for header in headers])} |\n"
-        for row in rows:
-            escaped_row = [escape_markdown(cell) for cell in row]
-            table += f"| {' | '.join(escaped_row)} |\n"
-        return table
-
-    def format_data(header: str, data: dict):
-        if header == "choices":
-            choices = [choice.get("name", "") for choice in data.get(header, [])]
-            choices_str = ", ".join(choices)
-            return f"`{choices_str}`"
-
-        return str(data.get(header, ""))
-
-    def generate_table_data(data: list):
-        headers = []
-        for attr in data:
-            for k in attr.keys():
-                if k not in headers:
-                    headers.append(k)
-
-        rows = []
-        for attr in data:
-            row = [format_data(header, attr) for header in headers]
-            rows.append(row)
-
-        return headers, rows
-
-    def generate_node_data(node: dict):
-        node_markdown = []
-        node_markdown.append(f"#### {node.get('name')}\n")
-        if node.get("description"):
-            node_markdown.append(f"- **Description:** {node.get('description')}")
-        if node.get("label"):
-            node_markdown.append(f"- **Label:** {node.get('label', '')}")
-        if node.get("icon"):
-            node_markdown.append(f"- **Icon:** {node.get('icon', '')}")
-        if node.get("menu_placement"):
-            node_markdown.append(
-                f"- **Menu Placement:** {node.get('menu_placement', '')}"
-            )
-        node_markdown.append(
-            f"- **Include in Menu:** {'✅' if node.get('include_in_menu') else '❌'}\n"
-        )
-        if node.get("order_by") or node.get("uniqueness_constraints"):
-            node_markdown.append("#### Ordering and Constraints\n")
-            node_markdown.append(
-                f"- **Order By:**{', '.join(node.get('order_by', []))}"
-            )
-            node_markdown.append(
-                f"- **Uniqueness Constraints:**{', '.join([' + '.join(c) for c in node.get('uniqueness_constraints', [])])}\n"
-            )
-
-        if attributes := node.get("attributes", []):
-            node_markdown.append("##### Attributes")
-            attribute_headers, attribute_rows = generate_table_data(attributes)
-            node_markdown.append(format_table(attribute_headers, attribute_rows))
-
-        if relationships := node.get("relationships", []):
-            node_markdown.append("#### Relationships")
-            relationship_headers, relationship_rows = generate_table_data(relationships)
-            node_markdown.append(format_table(relationship_headers, relationship_rows))
-        return node_markdown
-
     for _, file_values in schema_definition_files.items():
         content.append(f"- **Version:** {file_values['version']}\n")
 
         if generics := file_values.get("generics", []):
-            content.append("### Generics\n")
-            for generic in generics:
-                content.extend(generate_node_data(generic))
+            content.extend(handle_generics(generics))
 
         if nodes := file_values.get("nodes", []):
-            content.append("### Nodes\n")
-            for node in nodes:
-                content.extend(generate_node_data(node))
+            content.extend(handle_nodes(nodes))
 
         if extensions := file_values.get("extensions", []):
-            content.append("### Extensions\n")
-            for node in extensions.get("nodes", []):
-                content.append(f"#### {node.get('kind', '')}\n")
-
-                if attributes := node.get("attributes", []):
-                    content.append("#### Attributes")
-                    headers, rows = generate_table_data(attributes)
-                    content.append(format_table(headers, rows))
-
-                if relationships := node.get("relationships", []):
-                    content.append("#### Relationships")
-                    headers, rows = generate_table_data(relationships)
-                    content.append(format_table(headers, rows))
+            content.extend(handle_extensions(extensions))
 
     # Write README.md
     readme_path = extension_dir / "README.md"
