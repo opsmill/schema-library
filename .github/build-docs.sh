bbe709e7054a4c7518118ecf090ca191bfa356bf
Pete Crocker <pete@petecrocker.com>
2025-09-28 19:58:46 +0100

    update node

--- .github/build-docs.sh
+++ .github/build-docs.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# Try to use nvm to set Node.js version if available
+if [ -f "$HOME/.nvm/nvm.sh" ]; then
+  . "$HOME/.nvm/nvm.sh"
+  nvm install 20
+  nvm use 20
+fi
+
 cd docs && npm install && npm run build
