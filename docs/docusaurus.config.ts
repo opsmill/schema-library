cb5c2bcf3d46f02c249efa9ad5e613dcc82952d6
Pete Crocker <pete@petecrocker.com>
2025-10-07 10:03:11 +0100

    update docusaurus

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,8 +17,13 @@ const config: Config = {
   projectName: 'schema-library',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
   onDuplicateRoutes: "throw",
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+    },
+  },
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
